b5191849935411978adaaa6316b3ddfc734c981d
Julia Romanova <julia.v.romanova@gmail.com>
2021-11-15 16:45:18 +0300

    add comment for linter not being angry

--- internal/database/lock.go
+++ internal/database/lock.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+// AcquireTryLock - return true if lock is possible
 func AcquireTryLock(ctx context.Context, tx *sqlx.Tx, lockID int32) (bool, error) {
 	var isAcquired bool
 	err := tx.GetContext(ctx, &isAcquired, fmt.Sprintf("select pg_try_advisory_xact_lock(%d)", lockID))
